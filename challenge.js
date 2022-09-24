e9666b2e0195345ef867f504f1b6c9e5eff4a9f2
Becky449 <82038825+Becky449@users.noreply.github.com>
2022-09-24 09:12:08 +0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -10,10 +10,81 @@ class LinkedList {
 
   add(number) {
     // your code here
+  this.length++;
+  let newNode = new Node(number);
+  if (this.next) {
+  this.next.next = newNode;
+  this.next = newNode;
+  return newNode;
+  }
+  this.head = this.next = newNode;
+  return newNode;
+  }
+  
+  addAt(index, item) {
+  if (index == 0) {
+  this.length++;
+  let newNode = new Node(item);
+  if (this.head) {
+  this.head.next = newNode;
+  this.head = newNode;
+  return newNode;
+  }
+  
+  this.head = this.next = newNode;
+  return newNode;
+  }
+  
+  let previous = null;
+  let current = this.head;
+  for (let i = 0; i < index; i++) {
+  previous = current;
+  current = current.next;
+  }
+  
+  let newNode = new Node(item);
+  newNode.next = current;
+  previous.next = newNode;
+  
+  this.length++;
+  return newNode;
   }
 
   get(index) {
     // your code here
+  let initial = this.head;
+  let counter = 0;
+  while(initial) {
+  if(counter == index) {
+  return(initial.value)
+  }
+  counter++;
+  initial = initial.next;
+  }
+  return null;
+  }
+  
+  remove(index) {
+  if (index == 0) {
+  if(this.head) {
+  this.length++;
+  let del = this.head;
+  this.head = this.head.next;
+  return del;
+  }
+  return null;
+  }
+  
+  let prev = null;
+  let cur = this.head;
+  
+  for (let i = 0; i < index; i++); {
+  prev = cur;
+  cur = cur.next;
+  }
+  prev.next = cur.next;
+  this.length--;
+  return cur;
   }
 }
 
